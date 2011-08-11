0addb9930ceedbe66be9d30ef5ca1e845a0893bd
Rouslan Placella <rouslan@placella.com>
2011-08-11 11:53:07 +0100

    Updated CSS for navigation frame

--- themes/original/css/theme_left.css.php
+++ themes/original/css/theme_left.css.php
@@ -43,19 +43,10 @@ a:active {
     color:              #0000FF;
 }
 
-ul {
-    margin:0;
-}
-
 form {
     margin:             0;
     padding:            0;
     display:            inline;
-}
-
-select#select_server,
-select#lightm_db {
-    width:              100%;
 }
 
 /* buttons in some browsers (eg. Konqueror) are block elements,
@@ -78,29 +69,68 @@ button {
 /******************************************************************************/
 /* specific elements */
 
+div#pmalogo {
+    <?php //better echo $GLOBALS['cfg']['logoBGC']; ?>
+    background-color: <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
+    padding:.3em;
+}
+
+select#select_server,
+div#recentTableList select {
+    width: 100%;
+}
+
+div#pmalogo,
+div#leftframelinks,
+div#serverChoice,
+div#recentTableList,
+div#navidbpageselector {
+    text-align: center;
+    margin: 0.5em 1em 0 1em;
+}
+
+div#navidbpageselector a {
+    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
+    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
+}
+
+div#leftframelinks a img.icon {
+    margin:             0;
+    border:             0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
+    padding:            2px;
+}
+
+/* Navigation tree*/
 #navigation_tree {
+    margin: 0.5em 1em 0 1em;
     color: black;
-	padding: 1em 0 0 0;
 }
 #navigation_tree a {
-    color: black;
+    color: <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
 }
 #navigation_tree a:hover {
+    color: <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
     text-decoration: underline;
 }
 #navigation_tree ul {
-    padding-left: 0.5em;
-    padding-bottom: 0.2em;
-    margin-bottom: 0.1em;
+    padding-<?php echo $left; ?>: 0;
+    margin-bottom: 0.2em;
     list-style-type: none;
 }
 #navigation_tree ul ul {
-    border-left: 1px dotted gray;
-    border-bottom: 1px dotted gray;
+    padding-<?php echo $left; ?>: 0.5em;
+    border-<?php echo $left; ?>: 1px dotted <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
+    border-bottom: 1px dotted <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
 }
 #navigation_tree img {
 	margin: 0;
 }
+/* FIXME
+#navigation_tree ul li a:hover {
+    background: <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
+    color: <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
+}
+*/
 
 /* Button for collapsing the frame */
 div#collapse_frame .collapse_top {
@@ -122,184 +152,4 @@ div#collapse_frame div div {
     font-weight: bold;
     background: #ccc;
     position: relative;
-}
-
-div#pmalogo {
-    <?php //better echo $GLOBALS['cfg']['logoBGC']; ?>
-    background-color: <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-    padding:.3em;
-}
-
-div#recentTableList,
-div#serverChoice {
-    text-align: center;
-    margin: 0.5em 1em 0 1em;
-}
-
-div#recentTableList select {
-    width: 100%;
-}
-
-div#pmalogo,
-div#leftframelinks,
-div#databaseList {
-    text-align:         center;
-    margin-bottom:      0.5em;
-    padding-bottom:     0.5em;
-}
-
-ul#databaseList {
-    margin-bottom:      0.5em;
-    padding-bottom:     0.5em;
-    padding-<?php echo $left; ?>:     1.5em;
-    font-style: italic;
-}
-
-ul#databaseList a {
-    display: block;
-    font-style: normal;
-}
-
-div#navidbpageselector a,
-ul#databaseList a {
-    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-}
-
-ul#databaseList ul {
-    padding-left: 1em;
-    padding-right: 0;
-}
-
-ul#databaseList a:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-
-ul#databaseList li.selected a {
-    background: <?php echo $GLOBALS['cfg']['BrowseMarkerBackground']; ?>;
-    color: <?php echo $GLOBALS['cfg']['BrowseMarkerColor']; ?>;
-}
-
-div#leftframelinks .icon {
-    padding:            0;
-    margin:             0;
-}
-
-div#leftframelinks a img.icon {
-    margin:             2px;
-    border:             0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    padding:            0;
-}
-
-div#leftframelinks a:hover img {
-    background-color:         <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-
-/* serverlist */
-#body_leftFrame #list_server {
-    list-style-image: url(<?php echo $GLOBALS['cfg']['ThemePath']; ?>/original/img/s_host.png);
-    list-style-position: inside;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-}
-
-#body_leftFrame #list_server li {
-    margin: 0;
-    padding: 0;
-    font-size:          80%;
-}
-
-div#left_tableList ul {
-    list-style-type:    none;
-    list-style-position: outside;
-    margin:             0;
-    padding:            0;
-    font-size:          80%;
-    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-}
-
-div#left_tableList ul ul {
-    font-size:          100%;
-}
-
-div#left_tableList a {
-    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    text-decoration:    none;
-}
-
-div#left_tableList a:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    text-decoration:    underline;
-}
-
-div#left_tableList li {
-    margin:             0;
-    padding:            0;
-    white-space:        nowrap;
-}
-
-<?php if ($GLOBALS['cfg']['BrowseMarkerColor']) { ?>
-/* marked items */
-div#left_tableList > ul li.marked > a,
-div#left_tableList > ul li.marked {
-    background: <?php echo $GLOBALS['cfg']['BrowseMarkerBackground']; ?>;
-    color: <?php echo $GLOBALS['cfg']['BrowseMarkerColor']; ?>;
-}
-<?php } ?>
-
-<?php if ($GLOBALS['cfg']['LeftPointerEnable']) { ?>
-div#left_tableList > ul li:hover > a,
-div#left_tableList > ul li:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-<?php } ?>
-
-div#left_tableList img {
-    padding:            0;
-    vertical-align:     middle;
-}
-
-div#left_tableList ul ul {
-    margin-<?php echo $left; ?>:        0;
-    padding-<?php echo $left; ?>:       0.1em;
-    border-<?php echo $left; ?>:        0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    padding-bottom:     0.1em;
-    border-bottom:      0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-}
-
-/* for the servers list in navi panel */
-#serverinfo .item {
-    white-space:        nowrap;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-}
-#serverinfo a:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-
-#NavFilter {
-    display: none;
-}
-
-#clear_fast_filter {
-    background: white;
-    color: black;
-    cursor: pointer;
-    padding: 0;
-    margin: 3px 5px 0 -23px;
-    position: relative;
-    float: right;
-}
-
-#fast_filter {
-    width: 100%;
-    padding: 2px 0;
-    margin: 0;
-    border: 0;
 }
--- themes/pmahomme/css/theme_left.css.php
+++ themes/pmahomme/css/theme_left.css.php
@@ -44,19 +44,10 @@ a:active {
     color:              #0000FF;
 }
 
-ul {
-    margin:0;
-}
-
 form {
     margin:             0;
     padding:            0;
     display:            inline;
-}
-
-select#select_server,
-select#lightm_db {
-    width:              100%;
 }
 
 /* buttons in some browsers (eg. Konqueror) are block elements,
@@ -76,37 +67,70 @@ button {
 	cursor: pointer;
 }
 
-.navi_dbName {
-    font-weight:    bold;
-    color:          <?php echo $GLOBALS['cfg']['NaviDatabaseNameColor']; ?>;
-}
-
 /******************************************************************************/
 /* specific elements */
 
+div#pmalogo {
+    <?php //better echo $GLOBALS['cfg']['logoBGC']; ?>
+}
+
+select#select_server,
+div#recentTableList select {
+    width: 100%;
+}
+
+div#pmalogo,
+div#leftframelinks,
+div#serverChoice,
+div#recentTableList,
+div#navidbpageselector {
+    color: #333;
+    text-align: center;
+    margin: 5px 10px 0px 10px;
+}
+
+div#navidbpageselector a,
+div#navidbpageselector select{
+    color: <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
+    margin: 0.2em;
+}
+
+div#leftframelinks a img.icon {
+    margin: 0.3em;
+    border: 0px;
+}
+
+/* Navigation tree*/
 #navigation_tree {
+    margin: 5px 10px 0px 10px;
     color: #444;
-	padding: 1em 0 0 0;
 }
 #navigation_tree a {
-    color: #444;
+    color: <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
 }
 #navigation_tree a:hover {
+    color: <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
     text-decoration: underline;
 }
 #navigation_tree ul {
-    padding-left: 0.5em;
-    padding-bottom: 0.2em;
-    margin-bottom: 0.1em;
+    padding-<?php echo $left; ?>: 0;
+    margin-bottom: 0.2em;
     list-style-type: none;
 }
 #navigation_tree ul ul {
-    border-left: 1px dotted gray;
-    border-bottom: 1px dotted gray;
+    padding-<?php echo $left; ?>: 0.5em;
+    border-<?php echo $left; ?>: 1px dotted <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
+    border-bottom: 1px dotted <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
 }
 #navigation_tree img {
 	margin: 0;
 }
+/* FIXME
+#navigation_tree ul li a:hover {
+    background: <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
+    color: <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
+}
+*/
 
 /* Button for collapsing the frame */
 div#collapse_frame .collapse_top {
@@ -132,236 +156,4 @@ div#collapse_frame div div {
     -webkit-box-shadow: 0 0 2px 0 #444;
     -moz-box-shadow: 0 0 2px 0 #444;
     box-shadow: 0 0 2px 0 #444; 
-}
-
-div#pmalogo {
-    <?php //better echo $GLOBALS['cfg']['logoBGC']; ?>
-}
-
-div#recentTableList,
-div#serverChoice {
-    text-align: center;
-    margin: 0.5em 1em 0 1em;
-}
-
-div#recentTableList select {
-    width: 100%;
-}
-
-div#pmalogo,
-div#leftframelinks,
-div#databaseList {
-    text-align:         center;
-    margin:             5px 10px 0px 10px;
-}
-
-ul#databaseList {
-    margin: 0.8em 0px;
-    padding-bottom:     0.5em;
-    padding-<?php echo $left; ?>:     0.3em;
-    font-style: italic;
-}
-
-ul#databaseList span {
-    padding: 5px;
-}
-
-ul#databaseList a {
-    color: #333;
-    background: url(./themes/pmahomme/img/database.png) no-repeat 0 5px transparent;
-    display: block;
-    text-indent: 0;
-    padding: 5px 5px 5px 25px;
-    font-style: normal;
-}
-
-div#navidbpageselector {
-    margin: 0.1em;
-    text-align: center;
-}
-
-div#navidbpageselector a,
-div#navidbpageselector select{
-    color: #333;
-    margin: 0.2em;
-}
-
-ul#databaseList ul {
-    margin: 0;
-    padding: 0;
-}
-
-ul#databaseList li {
-    list-style: none;
-    text-indent: 20px;
-    margin: 0;
-    padding: 0;
-}
-
-ul#databaseList a:hover {
-    background-color: #e4e4e4;
-}
-
-ul#databaseList li.selected a {
-    background: <?php echo $GLOBALS['cfg']['BrowseMarkerBackground']; ?>;
-    color: <?php echo $GLOBALS['cfg']['BrowseMarkerColor']; ?>;
-}
-
-div#leftframelinks .icon {
-    padding:            0;
-    margin:             0;
-}
-
-div#reloadlink a img,
-div#leftframelinks a img.icon {
-    margin:        0.3em;
-    margin-top:    0.7em;
-    border:        0;
-}
-
-div#leftframelinks a:hover img {
-
-}
-
-/* serverlist */
-#body_leftFrame #list_server {
-    list-style-image: url(<?php echo $GLOBALS['cfg']['ThemePath']; ?>/original/img/s_host.png);
-    list-style-position: inside;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-}
-
-#body_leftFrame #list_server li {
-    margin: 0;
-    padding: 0;
-}
-
-div#left_tableList {margin:10px 10px 0 10px;}
-div#left_tableList ul {
-    list-style-type:    none;
-    list-style-position: outside;
-    margin:             0;
-    padding:            0;
-}
-
-div#left_tableList ul ul {
-    font-size:          100%;
-}
-
-div#left_tableList a {
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    text-decoration:    none;
-}
-
-div#left_tableList a:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    text-decoration:    underline;
-}
-
-div#left_tableList li {
-    margin:             0;
-    padding:            2px 0;
-    white-space:        nowrap;
-}
-
-#newtable {
-    margin-top: 15px !important;
-}
-
-#newtable a {
-    display: block;
-    padding: 1px;
-    <?php echo $_SESSION['PMA_Theme']->getCssGradient('ffffff', 'cccccc'); ?>
-    border: 1px solid #aaa;
-    -moz-border-radius: 20px;
-    -webkit-border-radius: 20px;
-    border-radius: 20px;
-}
-
-#newtable li:hover {
-    background: transparent !important;
-}
-
-#newtable a:hover {
-    <?php echo $_SESSION['PMA_Theme']->getCssGradient('cccccc', 'dddddd'); ?>
-}
-
-#newtable li a:hover {
-    text-decoration: none;
-}
-
-select{
-    -moz-border-radius:2px 2px 2px 2px;
-    -moz-box-shadow:0 1px 2px #DDDDDD;
-    border:1px solid #aaa;
-    color:#333333;
-    padding:3px;
-    background:url(./themes/pmahomme/img/input_bg.gif);
-}
-
-<?php if ($GLOBALS['cfg']['BrowseMarkerColor']) { ?>
-/* marked items */
-div#left_tableList > ul li.marked > a,
-div#left_tableList > ul li.marked {
-    background: #e4e4e4;
-    color: <?php echo $GLOBALS['cfg']['BrowseMarkerColor']; ?>;
-}
-<?php } ?>
-
-<?php if ($GLOBALS['cfg']['LeftPointerEnable']) { ?>
-div#left_tableList > ul li:hover > a,
-div#left_tableList > ul li:hover {
-    background:         #e4e4e4;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-<?php } ?>
-
-div#left_tableList img {
-    padding:            0;
-    vertical-align:     middle;
-}
-
-div#left_tableList ul ul {
-    margin-<?php echo $left; ?>:        0;
-    padding-<?php echo $left; ?>:       0.1em;
-    border-<?php echo $left; ?>:        0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-    padding-bottom:     0.1em;
-    border-bottom:      0.1em solid <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-}
-
-/* for the servers list in navi panel */
-#serverinfo .item {
-    white-space:        nowrap;
-    color:              <?php echo $GLOBALS['cfg']['NaviColor']; ?>;
-}
-#serverinfo a:hover {
-    background:         <?php echo $GLOBALS['cfg']['NaviPointerBackground']; ?>;
-    color:              <?php echo $GLOBALS['cfg']['NaviPointerColor']; ?>;
-}
-#reloadlink {
-    clear: both;
-    float: <?php echo $right; ?>;
-    display: block;
-    padding: 1em;
-}
-
-#NavFilter {
-    display: none;
-}
-
-#clear_fast_filter {
-    background: white;
-    color: black;
-    cursor: pointer;
-    padding: 0;
-    margin: 0;
-    position: relative;
-    right: 3ex;
-}
-
-#fast_filter {
-    width: 85%;
-    padding: 0.1em;
 }
